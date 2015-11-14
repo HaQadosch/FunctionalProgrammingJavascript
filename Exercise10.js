da7e9038d71cda78d9ae86fba04c591ce2658d78
HaQadosch <haqadosch@gmail.com>
2015-11-14 17:25:47 +0000

    exo 10 remove spaces

--- Exercise10.js
+++ Exercise10.js
@@ -12,4 +12,3 @@ Array.prototype.concatAll = function() {
 
 // JSON.stringify([ [1,2,3], [4,5,6], [7,8,9] ].concatAll()) === "[1,2,3,4,5,6,7,8,9]"
 // [1,2,3].concatAll(); // throws an error because this is a one-dimensional array
-		
