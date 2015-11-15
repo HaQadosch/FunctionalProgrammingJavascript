0fb6cf8edcb2c9ca3abb83b849dcb5c1894ea6a0
HaQadosch <haqadosch@gmail.com>
2015-11-15 18:26:59 +0000

    Exo 18 valid

---
+++ Exercise18.js
@@ -0,0 +1,14 @@
+(function() {
+	var boxarts = [
+		{ width: 200, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture200.jpg" },
+		{ width: 150, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture150.jpg" },
+		{ width: 300, height:200, url:"http://cdn-0.nflximg.com/images/2891/Fracture300.jpg" },
+		{ width: 425, height:150, url:"http://cdn-0.nflximg.com/images/2891/Fracture425.jpg" }
+	];
+
+	// You should return an array containing only the URL of the largest box art. Remember that reduce always
+	// returns an array with one item.
+	return boxarts.reduce(function(boxart, size){
+		return (boxart.width*boxart.height) > size ? boxart.width*boxart.height : size
+	}, 0).map(boxart => boxart.url);
+}());
